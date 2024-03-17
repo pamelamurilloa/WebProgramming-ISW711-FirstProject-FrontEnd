6937df2a5afc755cb4eba69f82f0ef40d5387f55
Pamela Murillo <pamelamurilloa@gmail.com>
2024-03-17 16:05:24 -0600

    Style finished, javascript added to get the pin and id, ready for checking

--- scripts/avatar-entry.js
+++ scripts/avatar-entry.js
@@ -1,13 +1,28 @@
 const profiles = document.querySelectorAll('.profile');
 const popup = document.getElementById('popup');
 const closeBtn = document.getElementById('close');
+const submitBtn = document.getElementById('submit-pin');
 
 profiles.forEach(profile => {
   profile.addEventListener('click', () => {
     popup.style.display = 'block';
+
+    const profileId = profile.id;
+    popup.setAttribute('data-profile-id', profileId);
   });
 });
 
 closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+// Handle PIN submission
+submitBtn.addEventListener('click', () => {
+  const enteredPIN = document.getElementById('pin').value;
+  const profileId = popup.getAttribute('data-profile-id');
+
+  console.log(enteredPIN + ", " + profileId);
+
+  //send pin to backend
+  }
+);
