00bbb19d23c3b09fc98a57240e5226f3a4a6ae8e
Pamela Murillo <pamelamurilloa@gmail.com>
2024-03-20 18:58:35 -0600

    VerifyUser not working, i commented it for future reference

--- scripts/security.js
+++ scripts/security.js
@@ -2,20 +2,25 @@
 const verifyUser = () => {
     const status = localStorage.getItem("admin");
     const userId = localStorage.getItem("userId");
+    console.log(status + ", " + userId);
 
-    if (!status && userId) {
-        window.location.replace("../pages/avatar-entry.html");
+    if (status == "false" && userId && userId.length > 0) {
+        if (window.location.href !="../pages/avatar-entry.html") {
+            window.location.replace("../pages/avatar-entry.html");
+        }
     } else if (!status && !userId){
-        window.location.replace("../index.html");
+        if (window.location.href !="../pages/login.html" && window.location.href !="../pages/register.html") {
+            window.location.replace("../index.html");
+        }
     }
 }
 
 const logout = () => {
     localStorage.clear();
-    window.location.replace("../index.html");
+    verifyUser();
   }
 
 const partialLogOut = () => {
     localStorage.setItem("admin", false);
-    window.location.replace("../pages/avatar-entry.html");
+    verifyUser();
 }
